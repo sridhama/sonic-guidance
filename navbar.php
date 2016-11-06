89d5f7db6c51190d4e4d620ad2313744531f724c
Sridhama Prakhya <sridhama@sridhama.com>
2016-11-06 09:31:40 -0600

    nope

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <nav class="cyan darken-2" role="navigation" style="height: 100px;">
   <div class="nav-wrapper container" style="padding-top:20px;"><a id="logo-container" href="./" class="brand-logo"><img src="./logo.png" style="filter: invert(100%);" height="60px"></a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="./index"> Home</a></li>
+      <li><a href="./"> Home</a></li>
       <li><a href="./about"> About</a></li>
       <li><a href="./data"> Data</a></li>
     </ul>
